7be4c7a721c02a012494a7f20366b4c051be6f88
jcaceresap <jcaceresap@unsa.edu.pe>
2024-05-05 10:08:52 -0500

    terminando ej1

--- lab02/ej1.py
+++ lab02/ej1.py
@@ -1,17 +1,23 @@
 def main():
     masa1 = float(input("Escriba la masa del objeto 1 (kg): "))
     masa2 = float(input("Escriba la masa del objeto 2 (kg): "))
+    aceleracion = calcular_aceleracion(masa1, masa2)
     tension = AtwoodMachine(masa1, masa2)
-    print("Los objetos con sus masas ingresadas tiene una tension ")
+    print("Los objetos con sus masas ingresadas tiene una aceleracion de", aceleracion, "m/s^2 y una tension de", tension, "N")
 
 def AtwoodMachine(masa1, masa2):
+    #Calcular tension
+    gravedad = 9.81
+    aceleracion = calcular_aceleracion(masa1, masa2)
+    tension = masa1 * gravedad - masa1 * aceleracion 
+    return tension
+
+def calcular_aceleracion(masa1, masa2):
     #Calcular aceleracion
     gravity = 9.81
     numerador = (masa1 - masa2) * gravity
     denominador = masa1 + masa2 
     aceleracion = numerador / denominador
-    #Calcular tension
-    tension = masa1 * gravity - masa1 * aceleracion 
-    return tension
+    return aceleracion
 
-def calcular_aceleracion(masa1, masa2):
+main()
