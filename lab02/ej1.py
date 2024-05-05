642d7d54e814f48c44c09fcc9a98690cfe182d71
jcaceresap <jcaceresap@unsa.edu.pe>
2024-05-05 10:12:05 -0500

    arreglando caso especial

--- lab02/ej1.py
+++ lab02/ej1.py
@@ -1,9 +1,12 @@
 def main():
     masa1 = float(input("Escriba la masa del objeto 1 (kg): "))
     masa2 = float(input("Escriba la masa del objeto 2 (kg): "))
-    aceleracion = calcular_aceleracion(masa1, masa2)
-    tension = AtwoodMachine(masa1, masa2)
-    print("Los objetos con sus masas ingresadas tiene una aceleracion de", aceleracion, "m/s^2 y una tension de", tension, "N")
+    if (masa1 > masa2):
+        aceleracion = calcular_aceleracion(masa1, masa2)
+        tension = AtwoodMachine(masa1, masa2)
+        print("Los objetos con sus masas ingresadas tiene una aceleracion de", aceleracion, "m/s^2 y una tension de", tension, "N")
+    else:
+        print("Las masas ingresadas no son válidas (m1 > m2).")
 
 def AtwoodMachine(masa1, masa2):
     #Calcular tension
