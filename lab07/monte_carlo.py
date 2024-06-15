23259ea25dc387f472d79e6d9c65b6ae663f4d77
jcaceresap <jcaceresap@unsa.edu.pe>
2024-06-15 18:46:35 -0500

    lab07

---
+++ lab07/monte_carlo.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Parámetros y valor teórico
+valueTeo = 0.58904862554
+a, b = 0, 1
+N = 100000
+print('Integral:')
+
+def monte_carlo(f, a, b, n):
+    x = np.random.uniform(a, b, n)
+    fx = f(x)
+    integral = (b - a) * np.mean(fx)
+    return integral
+
+def f(x):
+    return (1 - x**2)**(3/2)
+
+res = monte_carlo(f, a, b, N)
+
+relative_error = abs((res - valueTeo) / valueTeo)
+
+error_teorico = 1 / np.sqrt(N)
+
+print(f'Valor Monte Carlo: {res:.8f}')
+print(f'Valor Teórico: {valueTeo:.8f}')
+print(f'Error Relativo: {relative_error:.8f}')
+print(f'Error Teórico: {error_teorico:.8f}')
+
